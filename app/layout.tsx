7883d351ee142a4cb5ef2cab2039a9c51c0bcf70
pratikgrv <pratikgaurav37@gmail.com>
2023-12-05 17:21:02 +0530

    metadata change required

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,41 @@ import "@/styles/globals.css";
 import { cn } from "@/libs/utils";
 import QueryProvider from "@/providers/query-provider";
 import { ThemeProvider } from "@/providers/theme-provider";
-
+import { siteConfig } from "@/config/site";
+import { Metadata } from "next";
 
 const inter = Roboto_Mono({ subsets: ["latin"] });
+
+export const metadata:Metadata = {
+	title: {
+		default: siteConfig.name,
+		template: `%s | ${siteConfig.name}`,
+	},
+	description: siteConfig.description,
+	keywords: [
+		"NFT calander",
+		"trending nfts",
+		"Popular NFTs",
+		"nft mints",
+		"best crypto nft",
+	],
+	
+	creator: "pratikgrv",
+	themeColor: [
+		{ media: "(prefers-color-scheme: light)", color: "white" },
+		{ media: "(prefers-color-scheme: dark)", color: "black" },
+	],
+	openGraph: {
+		type: "website",
+		locale: "en_US",
+		url: siteConfig.url,
+		title: siteConfig.name,
+		description: siteConfig.description,
+		siteName: siteConfig.name,
+	},
+	
+};
+
 
 export default function RootLayout({
 	children,
---
+++ config/site.ts
@@ -0,0 +1,12 @@
+
+
+export const siteConfig = {
+	name: "Zenl",
+	description:
+		"A platform for finding upcoming and trending NFTs in web3",
+	url: "https://zenl.com",
+	ogImage: "",
+	links: {
+		twitter: "",
+	},
+};
