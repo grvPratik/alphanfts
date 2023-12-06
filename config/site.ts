8b3a3ad3db8dd3a410a02fdd346da2f01a6a4da6
pratikgrv <pratikgaurav37@gmail.com>
2023-12-07 00:42:27 +0530

    env fixed

--- config/site.ts
+++ config/site.ts
@@ -2,12 +2,9 @@ export const siteConfig = {
 	name: "Alphanfts",
 	description: "A platform for finding upcoming and trending NFTs in web3",
 	url: "https://alphanfts.app",
-<<<<<<< HEAD
 	ogImage: "",
 	links: {
 		twitter: "",
 	},
-=======
-	
->>>>>>> df081d6aec9c49fd7c7410ad363dd58566ce9b34
+
 };
